c2273acf8271298d08b313a8f35c7a1df5e55b29
krishna <eemail.krishna@gmail.com>
2024-11-27 14:33:18 +0530

    change token expre time

--- config/jwt.js
+++ config/jwt.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 const { secret } = require('./config');
+require('dotenv').config();
+const expiry = process.env.JWT_EXPIRES_IN
 
 module.exports = {
-  sign: (payload) => jwt.sign(payload, secret, { expiresIn: '1h' }),
-  verify: (token) => jwt.verify(token, secret)
+  sign: (payload) => jwt.sign(payload, secret, { expiresIn: expiry }),
+  verify: (token) => jwt.verify(token, secret),
 };
